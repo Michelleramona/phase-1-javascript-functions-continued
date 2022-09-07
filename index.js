96fae45876816565a9e732ca04fabefa9ca7bcc3
Michelle Vitello <michelleramona89@gmail.com>
2022-09-07 10:05:34 -0400

    commit tree cbda307c6a2a5cb05cbe3f92f2d2fcc3338ebf09

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun (activity = "roller-skate"){
+return `This Saturday, I want to ${activity}!`
+}
+
+//but is asking for function expression not function declaration - what does ƒ mean
+//how do I use it properly?
+
+function mondayWork (work= "go to the office"){
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(worker) {
+    return `You are *${worker}*` 
+}
+
+function wrapAdjective(flair = "*") {
+    const adjective = function (worker = "special"){
+    return `You are ${flair}${worker}${flair}!`;
+    }
+    return adjective;
+  }
+  
